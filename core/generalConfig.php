f7ef45dc32ce733e37d7e737370f97fc52642077
Byron Bustamante <byronbl.12@gmail.com>
2019-11-13 05:03:50 +0000

    Creamos la configuracion general y el htaccess para controlar el acceso

---
+++ core/generalConfig.php
@@ -0,0 +1,4 @@
+<?php
+const SERVERURL = 'http://localhost/app_pedidos/';
+const COMPANY = 'TIENDA ONLINE';
+date_default_timezone_set('Europe/Madrid');
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
+require_once 'core/generalConfig.php';
 include 'views/template.php';
