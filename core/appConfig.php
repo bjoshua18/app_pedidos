1c882b4dcc02fa121dada1a2bd54d922e4a08fc8
Byron Bustamante <byronbl.12@gmail.com>
2019-11-13 09:48:11 +0000

    Implementamos la encriptacion, funciones en el mainModel y la configuracion en appConfig

--- core/appConfig.php
+++ core/appConfig.php
@@ -1,7 +1,13 @@
 <?php
+// DB CONFIG
 const DBHOST = 'localhost';
 const DBNAME = 'app_pedidos';
 const DBUSER = 'root';
 const DBPASS = '';
 
 const DSN = 'mysql:host='.DBHOST.';dbname='.DBNAME;
+
+// ENCRYPTION CONFIG
+const METHOD = 'AES-256-CBC';
+const SECRET_KEY = '$TO@DSW@2019';
+const SECRET_IV = '13112019';
--- core/mainModel.php
+++ core/mainModel.php
@@ -3,7 +3,7 @@ $ajaxReq ? require_once '../core/appConfig.php' : require_once './core/appConfig
 
 class mainModel
 {
-	// DB Functions
+	// DB FUNCTIONS
 	protected function connect() {
 		return new PDO(DSN, DBUSER, DBPASS);
 	}
@@ -12,5 +12,23 @@ class mainModel
 		$resp = self::connect()->prepare($query);
 		$resp->execute();
 		return $resp;
+	}
+
+	// AUX FUNCTIONS
+
+	public function encryption($string) {
+		$output = false;
+		$key = hash('sha256', SECRET_KEY);
+		$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+		$output = openssl_encrypt($string, METHOD, $key, 0, $iv);
+		$output = base64_encode($output);
+		return $output;
+	}
+
+	protected function decryption($string) {
+		$key = hash('sha256', SECRET_KEY);
+		$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+		$output = openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+		return $output;
 	}
 }
