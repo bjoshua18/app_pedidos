33da9c46257d85192688eb8513e68e66fc6910fb
Byron Bustamante <byronbl.12@gmail.com>
2019-12-09 20:52:47 +0000

    Terminamos la funcionalidad de iniciar sesion

--- models/loginModel.php
+++ models/loginModel.php
@@ -3,5 +3,12 @@ $ajaxReq ? require_once '../core/mainModel.php' : require_once './core/mainModel
 
 class loginModel extends mainModel
 {
-
+	protected function login_model($data) {
+		$query = 'SELECT * FROM accounts WHERE AccountUser=:User AND AccountPassword=:Password AND AccountState="Active"';
+		$sql = mainModel::connect()->prepare($query);
+		$params = [':User', ':Password'];
+		mainModel::bindMultiplesParams($sql, $params, $data);
+		$sql->execute();
+		return $sql;
+	}
 }
--- views/template.php
+++ views/template.php
@@ -5,6 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="<?= SERVERURL ?>views/css/main.css">
+	<!--====== Scripts -->
+	<?php include 'views/modules/scripts.php' ?>
 </head>
 <body>
 	<?php
@@ -16,7 +18,7 @@
 		if($view == 'welcome' || $view == 'login' || $view == 'register' || $view == '404'):
 			require_once "./views/pages/$view-view.php";
 		else:
-			session_start();
+			session_start(['name' => 'AP']);
 	?>
 <!-- SideBar -->
 <?php include 'views/modules/sidebar.php' ?>
@@ -30,7 +32,5 @@
 	<?php require_once $view ?>
 </section>
 <?php endif ?>
-<!--====== Scripts -->
-<?php include 'views/modules/scripts.php' ?>
 </body>
 </html>
