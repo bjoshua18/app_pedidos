d7db60d036870a4fe62e71703cc84dd32d2f4cae
Byron Bustamante <byronbl.12@gmail.com>
2019-12-09 11:55:39 +0000

    Creamos la estructura MVC del login

---
+++ ajax/loginAjax.php
@@ -0,0 +1,2 @@
+<?php
+$ajaxReq = true;
---
+++ controllers/loginController.php
@@ -0,0 +1,7 @@
+<?php
+$ajaxReq ? require_once '../models/loginModel.php' : require_once './models/loginModel.php';
+
+class loginController extends loginModel
+{
+
+}
